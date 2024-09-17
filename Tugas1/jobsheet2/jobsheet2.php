5724f9c93ad49bca0fc1b8f7d3e64152be157b48
AdityaNugroho07 <168439946+AdityaNugroho07@users.noreply.github.com>
2024-09-17 15:57:15 +0700

    Add files via upload

---
+++ Tugas1/jobsheet2/jobsheet2.php
@@ -0,0 +1,139 @@
+<?php
+// 1. Class dan Object
+class Mahasiswa {
+    private $nama;
+    private $nim;
+    private $jurusan;
+
+    // Constructor
+    public function __construct($nama, $nim, $jurusan) {
+        $this->nama = $nama;
+        $this->nim = $nim;
+        $this->jurusan = $jurusan;
+    }
+
+    // 2. Encapsulation
+    public function getnama() {
+        return $this->nama;
+    }
+
+    public function setnama($nama) {
+        $this->nama = $nama;
+    }
+
+    public function getnim() {
+        return $this->nim;
+    }
+
+    public function setnim($nim) {
+        $this->nim = $nim;
+    }
+
+    public function getjurusan() {
+        return $this->jurusan;
+    }
+
+    public function setjurusan($jurusan) {
+        $this->jurusan = $jurusan;
+    }
+
+    public function tampilkandata() {
+        return "Nama: " . $this->nama . ", NIM: " . $this->nim . ", Jurusan: " . $this->jurusan;
+    }
+}
+
+// 3. Inheritance
+class Pengguna {
+    protected $nama;
+
+    public function __construct($nama) {
+        $this->nama = $nama;
+    }
+
+    public function getnama() {
+        return $this->nama;
+    }
+}
+
+class Dosen extends Pengguna {
+    private $mataKuliah;
+
+    public function __construct($nama, $mataKuliah) {
+        parent::__construct($nama);
+        $this->mataKuliah = $mataKuliah;
+    }
+
+    public function getmatakuliah() {
+        return $this->mataKuliah;
+    }
+
+    public function tampilkandatadosen() {
+        return "Nama: " . $this->nama . ", Mata Kuliah: " . $this->mataKuliah;
+    }
+}
+
+// 4. Polymorphism
+class PenggunaPolymorph {
+    protected $nama;
+
+    public function __construct($nama) {
+        $this->nama = $nama;
+    }
+
+    public function aksesfitur() {
+        return $this->nama . " mengakses fitur umum.";
+    }
+}
+
+class Dosenpoly extends PenggunaPolymorph {
+    public function aksesfitur() {
+        return $this->nama . " mengakses fitur dosen.";
+    }
+}
+
+class MahasiswaPoly extends PenggunaPolymorph {
+    public function aksesfitur() {
+        return $this->nama . " mengakses fitur mahasiswa.";
+    }
+}
+
+// 5. Abstraction
+abstract class PenggunaAbstraction {
+    protected $nama;
+
+    public function __construct($nama) {
+        $this->nama = $nama;
+    }
+    abstract public function aksesfitur();
+}
+
+class MahasiswaAbstraction extends PenggunaAbstraction {
+    public function aksesfitur() {
+        return $this->nama . " mengakses fitur mahasiswa.";
+    }
+}
+
+class DosenAbstraction extends PenggunaAbstraction {
+    public function aksesfitur() {
+        return $this->nama . " mengakses fitur dosen.";
+    }
+}
+
+$mahasiswa = new Mahasiswa("Aditya Nugroho", "230302073", "Teknik Informatika");
+echo $mahasiswa->tampilkandata() . "<br>";
+
+$dosen = new Dosen("Harry", "Matematika");
+echo $dosen->tampilkandatadosen() . "<br>";
+
+$dosenPoly = new DosenPoly("Harry");
+$mahasiswaPoly = new MahasiswaPoly("Aditya Nugroho");
+
+echo $dosenPoly->aksesfitur() . "<br>";
+echo $mahasiswaPoly->aksesfitur() . "<br>";
+
+$dosenAbstrak = new DosenAbstraction("Harry");
+$mahasiswaAbstrak = new MahasiswaAbstraction("Aditya Nugroho");
+
+echo $dosenAbstrak->aksesfitur() . "<br>";
+echo $mahasiswaAbstrak->aksesfitur() . "<br>";
+?>
---
+++ Tugas1/jobsheet3/jobsheet3.php
@@ -0,0 +1,92 @@
+<?php
+// 1. Inheritance
+class Person {
+    protected $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getname() {
+        return $this->name;
+    }
+}
+// 2. Polymorphism
+class Teacher extends Person {
+    private $teacherID;
+
+    public function __construct($name, $teacherID) {
+        parent::__construct($name);
+        $this->teacherID = $teacherID;
+    }
+
+    public function getteacherid() {
+        return $this->teacherID;
+    }
+
+    public function getname() {
+        return "Nama Dosen: " . $this->name;
+    }
+}
+
+class Student extends Person {
+    // 3. Encapsulation
+    private $studentID;
+    public function __construct($name, $studentID) {
+        parent::__construct($name);
+        $this->studentID = $studentID;
+    }
+
+    public function getstudentid() {
+        return $this->studentID;
+    }
+
+    public function getname() {
+        return "Nama Mahasiswa: " . $this->name;
+    }
+
+    public function setname($name) {
+        $this->name = $name;
+    }
+
+    public function setStudentID($studentID) {
+        $this->studentID = $studentID;
+    }
+}
+
+// 4. Abstraction
+abstract class Course {
+    protected $courseName;
+    public function __construct($courseName) {
+        $this->courseName = $courseName;
+    }
+    abstract public function getCourseDetails();
+}
+
+class OnlineCourse extends Course {
+    public function getCourseDetails() {
+        return "Kursus Online: " . $this->courseName;
+    }
+}
+
+class OfflineCourse extends Course {
+    public function getCourseDetails() {
+        return "Kursus Offline: " . $this->courseName;
+    }
+}
+
+$student = new Student("Aditya Nugroho", "230302073");
+echo $student->getName() . "<br>";
+echo "Student ID: " . $student->getStudentID() . "<br>";
+
+$teacher = new Teacher("Andi", "198765");
+echo $teacher->getName() . "<br>";
+echo "Teacher ID: " . $teacher->getTeacherID() . "<br>";
+
+$onlineCourse = new OnlineCourse("Pemrograman Web");
+$offlineCourse = new OfflineCourse("Bahasa Inggris");
+
+echo $onlineCourse->getCourseDetails() . "<br>";
+echo $offlineCourse->getCourseDetails() . "<br>";
+
+?>
